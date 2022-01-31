8c19ee5e8f5a254f099a95542860cf8d727cc236
ArtjomVinogradov <98739429+ArtjomVinogradov@users.noreply.github.com>
2022-01-31 11:35:37 +0200

    Add files via upload

---
+++ Python first try.py
@@ -0,0 +1,56 @@
+# Harjutus 01
+# Artjom Vinogradov
+# 31.01.22
+
+
+
+
+
+print("\"Cabernet Sauvignon\" või 'Cabernet Sauvignon'?")
+
+print("  @..@")
+print(" (----)")
+print("( \__/ )")
+print('^^ "" ^^')  
+
+a = int(input("Siseta ruudu külje pikkus: "))
+p = a * a
+print("vastus: ruudu pindala on",p,"cm^2")
+
+
+
+'''
+nimi = input("Lisa oma nimi: ")
+print("Tere", nimi+"!") #pluss ei lisa tühikut
+vanus = int(input("Lisa oma vanus: "))
+vanuste_vahe = 16-vanus
+print("Meie vanuste vahe on:",vanuste_vahe,"aastat")
+'''
+
+'''
+a=12
+b=16
+
+S=a*b
+P = 2*(a+b)
+print("pindala",S,"ruutmeetrit")
+print("ümbermõõt",P,"meetrit")
+print(S)
+print(P)
+'''
+
+
+
+
+'''
+print("tere maailm") #sõne
+print('tere sa ka') #String
+print(2000) #int / täisarv
+print(2.5) #float / komaarv
+print('ütle "tere maailm"')
+print("ütle \"tere maailm\"") #escape string
+
+print(type(2000)) # näitab andmetüüpi
+'''
+
+
