84509ace190dc3f7f2f9efc2f79f18f8c1b495a9
ArtjomVinogradov <98739429+ArtjomVinogradov@users.noreply.github.com>
2022-02-01 13:58:45 +0200

    Add files via upload

---
+++ Python 02.py
@@ -0,0 +1,48 @@
+# Harjutus 02
+# Artjom Vinogradov
+# 01.02.22
+
+
+
+
+
+
+
+
+
+
+#Pitsa
+hind = 12.90
+protsent = 10
+terve pitsa = 100
+vastus = (hind*protsent)/terve pitsa
+print(vastus, "Jätate teenindajale",vastus,"€")
+
+
+
+
+
+
+#3 toote hind
+hind = 36.75
+soodus = 0.4
+kogus = 3
+summa = (hind-(hind*soodus))*kogus
+print(kogus, "toote hind on",summa,"€")
+
+
+
+
+#kolmnurga ümbermõõt
+a,b,c = 15,16,18
+P=a+b+c
+print("Kolmnurga ümbermõõt: ",P)
+
+
+
+
+
+
+
+
+
