683836b668dcbad76a6f536bce89191c173f8348
ArtjomVinogradov <98739429+ArtjomVinogradov@users.noreply.github.com>
2022-02-01 14:00:39 +0200

    Add files via upload

--- Python 02.py
+++ Python 02.py
@@ -1,25 +1,60 @@
+import math
 # Harjutus 02
 # Artjom Vinogradov
 # 01.02.22
 
 
+#Arvutisüsteemid
+from math import *
+arv = int(input("Kirjuta arv: "))
+ruutjuur = sqrt(arv)
+ 
+print(ruutjuur)
 
 
 
 
+
+
+
+#Ajateisendus
+aeg = int(input("Kirjuta aeg: "))
+tunnid = aeg//60
+minutid = aeg%60
+print(tunnid,":",minutid)
+
+
+
+
+
+
+
+
+
+#Leia kolmnurga hüpotenuus
+a,b = 16,9
+c = math.sqrt((a*a) + (b*b))
+print("Kolmnurga hüpotenuus on: ",c)
+
+
+
+
+
+# Rulluisutajad
+kiirus = 29.9
+aeg = 24
+kaugel = (kiirus*aeg)
+print("ta jõuab: ",kaugel,"meetrit")
 
 
 
 
 #Pitsa
 hind = 12.90
-protsent = 10
-terve pitsa = 100
-vastus = (hind*protsent)/terve pitsa
+protsent = 0.1
+inimesed = 3
+vastus = ((hind*protsent)+hind)/inimesed
 print(vastus, "Jätate teenindajale",vastus,"€")
-
-
-
 
 
 
