3258a2eab92a759a69a27a3ff31469d8f0255adb
ArtjomVinogradov <98739429+ArtjomVinogradov@users.noreply.github.com>
2022-02-03 10:01:26 +0200

    Add files via upload

---
+++ Python 04.py
@@ -0,0 +1,37 @@
+
+# Harjutus 04
+# Artjom Vinogradov
+# 03.02.22
+
+
+#matemaatika
+arv1 = int(input("sisesta arv"))
+arv2 = int(input("sisesta arv mis sa ootad?"))
+tehe = int(input("vali tehe:\n 1) liitmine\n 2) lahutamine\n 3) jagamine\n 4) korrutamine\n"))
+if tehe==1:
+    vastus = arv1+arv2
+elif tehe==2:
+    vastus = arv1-arv2
+elif tehe==3:
+    vastus = arv1/arv2
+elif tehe==4:
+    vastus = arv1*arv2
+print(vastus)
+
+
+
+
+
+
+#Ruudu kontroll ==> < >= >= !=
+a = int(input("Sisesta ruudu üks külg: "))
+b = int(input("Sisesta ruudu teine külge: "))
+if a == b:
+    print("Tegemist on ruuduga")
+#elif:
+#     dsadasfd
+else:
+    print("Risttahukas")
+    
+    
+        
