c4a07fe8281c55c511ca5f18cd04875597c51b7c
ArtjomVinogradov <98739429+ArtjomVinogradov@users.noreply.github.com>
2022-02-09 13:59:59 +0200

    Add files via upload

--- Python 04.py
+++ Python 04.py
@@ -1,26 +1,118 @@
-
+from random import randint
 # Harjutus 04
 # Artjom Vinogradov
-# 08.02.22
+# 09.02.22
+
+
+#Arvamismäng
+nr = for i in range(1,21):
+loop = 1
+kordade_arv = 0
+
+print('Arva ära number 1-20')
+ 
+while loop == 1:
+    arva = int(input('Sisesta täisarv: '))
+    
+    if arva == nr:
+        kordade_arv += 1
+        print('Tubli, pakkumisi kokku: ',kordade_arv)
+        loop = 0
+    elif arva < nr:
+        kordade_arv += 1
+        print('Sinu pakutud arv on vale u noob')
+    else:
+        kordade_arv += 1
+        print('Sinu pakutud arv on good man')
+  
+
+
+
+
+
+
+#Viisikud
+for i in range(1,101):
+    if i%5==0:
+        print(i)
+   
+
+
+
+
+
+#Pisike korrutustabel
+arv = int(input("Siseta number: "))
+for i in range(1,11):
+   print(arv,'*',i,'=',arv*i)
+   
+
+
+
+
+
+
+
+#Paaris ja paaritu
+for i in range(1,101):
+    if i%2==0:
+        print(i,"paaris")
+    else:
+        print(i,"paaritu")
+
+
+
+
+
+#Loto
+for i in range(1,6):
+    print(randint(0,9),end="")
+print()
+
+
+
+
+#Tärnid teist version
+for i in range(1,6):
+    print("* "* i)
+
+j=5
+print("* "* j)
+
+
+
+
+
+
+#Tärnid
+def pattern(n):
+      k = 5 * n - 5
+      for i in range(n, -1,-1):
+           for j in range(k,0,-1):
+               print(end=" ")
+           k = k +2
+           for j in range(0, i+1):
+                print("*", end=" ")
+           print("\r")
+pattern(5)
+
+
+
+
 
 
 #jalgpalli meeskond
 
-sugu = int(input("Noh kes sa oled (M/N): "))
+sugu =input("Noh kes sa oled (M/N): ")
 
 if sugu == "M":
-    vanus = int(input("Ну сколько живешь ходишь по планете?"))
+    vanus = int(input("Ну сколько живешь ходишь по планете? "))
     if vanus>=16 and vanus<=18:
         print("so lets goo")
-    else :
+    else:
         print("No your are too old or too young")
 else:
     print("Naised ei sobi")
-
-
-
-    
-
 
 
 
