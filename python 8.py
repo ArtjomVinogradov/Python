75bcf7a7c7fee97aa504cf11507f0aaddbf9bb99
ArtjomVinogradov <98739429+ArtjomVinogradov@users.noreply.github.com>
2022-02-26 14:13:37 +0200

    Add files via upload

---
+++ python 8.py
@@ -0,0 +1,89 @@
+# Artjom Vinogradov
+#  26.02.2022
+#    IT-21
+
+
+
+#Esimene auto
+class Auto1:
+    #atribuudid
+    nimi = 'teadmata'
+    vanus = 0
+    hind = 'teadmata'
+    
+    
+    #meetodid
+    def __init__(self,x,y):
+        self.nimi = x
+        self.vanus = y
+       
+        
+    def kuva(self):
+        print('Nimi: {} \nVanus: {}'.format(self.nimi, self.vanus))
+        
+    def lisahind(self,x):
+        self.hind = x
+        
+    def lisamootor(self,x):
+        self.mootor = x
+        
+    def kuvamootor(self):
+        print('Mootor: {}'.format(self.mootor))
+        
+    def kuvahind(self):
+        print('Hind: {}'.format(self.hind))
+        
+
+uusObjekt = Auto1("Volkswagen (Touareg)", 2018)
+uusObjekt.lisahind('50 900€ "auto24.ee"')
+uusObjekt.kuva()
+uusObjekt.kuvahind()
+uusObjekt.lisamootor("3.0 TDI 210kW")
+uusObjekt.kuvamootor()
+
+
+
+
+
+#Teine auto
+class Auto2:
+    #atribuudid
+    nimi = 'teadmata'
+    vanus = 0
+    hind = 'teadmata'
+    mootor = 'teadmata'
+    
+    
+    #meetodid
+    def __init__(self,x,y):
+        self.nimi = x
+        self.vanus = y
+       
+        
+    def kuva(self):
+        print('Nimi: {} \nVanus: {}'.format(self.nimi, self.vanus))
+        
+    def lisahind(self,x):
+        self.hind = x
+        
+    def lisamootor(self,x):
+        self.mootor = x
+        
+    def kuvamootor(self):
+        print('Mootor: {}'.format(self.mootor))
+        
+    def kuvahind(self):
+        print('Hind: {}'.format(self.mootor))
+        
+
+uusObjekt = Auto1("BMW X5", 2018)
+uusObjekt.lisahind('49 900€ "auto24.ee"')
+uusObjekt.kuva()
+uusObjekt.kuvahind()
+uusObjekt.lisamootor("3.0 TDI 190kW")
+uusObjekt.kuvamootor()
+
+
+
+
+
