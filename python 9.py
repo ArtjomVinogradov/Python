f531224fe2c0f1bf1e63b2cc2a92fb59d9614de9
ArtjomVinogradov <98739429+ArtjomVinogradov@users.noreply.github.com>
2022-03-02 09:29:38 +0200

    Add files via upload

--- python 9.py
+++ python 9.py
@@ -8,15 +8,14 @@
 import datetime
 
 aeg = datetime.datetime.now()                                                                  #1980-11-06 17:32:56
-print(aeg.strftime("%d veebruar %Y aasta"))
+print(aeg.strftime("%d märts %Y aasta"))
 
 
 #isikukood
-kood = 50511020991
-aasta = int(kood[1]+kood[2])+2000
+kood = "50511020991"
+aasta = int(kood[1]+kood[2])
 kuu = int(kood[3]+kood[4])
-päev = int(kood[5]+kood[6])
+paev = int(kood[5]+kood[6])
 
-
-
-
+sp = datetime.date(aasta, kuu, paev)
+print(sp)
