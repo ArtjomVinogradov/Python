6d77d67be42e6eb8a8965c100db896f28bf917a1
ArtjomVinogradov <98739429+ArtjomVinogradov@users.noreply.github.com>
2022-03-15 00:27:01 +0200

    Add files via upload

---
+++ iseseisevtoo nr2.py
@@ -0,0 +1,19 @@
+import random
+
+a,b = random.randit(1,10)
+a,b = a+b
+a,b = 0
+
+
+for i in range(10):
+
+    if i==a+b
+        print(õige)
+    elif
+        print(vale)
+    
+        
+        
+        
+        
+        
