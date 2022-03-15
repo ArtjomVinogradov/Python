045b443f6bdb35683f2a68accce60ec64cbc65d6
ArtjomVinogradov <98739429+ArtjomVinogradov@users.noreply.github.com>
2022-03-15 21:26:24 +0200

    Add files via upload

---
+++ Iseseisev too.py
@@ -0,0 +1,30 @@
+import random
+
+õiged = 0
+
+for i in range(10):
+
+    tehe = random.randint(1)
+    a = random.randint(1,10)
+    b = random.randint(1,10)
+    if tehe==1:
+        prod = a * b
+        vastus = int(input(f"okay lets go man: {a}*{b}= "))
+        
+        if vastus==prod:
+            print("õige")
+            õiged+=1
+        else:
+            print("vale")
+            
+
+    
+            
+print("Õiged vastused: %s/10" %(õiged))
+
+per=int(õiged/10*100)
+
+if per>=50:
+    print(f"Väga hea, sa sai selle töö valmis {per} %")
+else:
+    print(f"Oh kurat mees, ära anna alla järgmine kord tee valmis {per} % ")
